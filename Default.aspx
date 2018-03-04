5ba77f3b1fa75129627cc383a2245868d31e5751
Krunal Bhadresha <krunalbhadresa@gmail.com>
2018-03-03 23:08:23 -0500

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label3" runat="server" Text="Tries left : "></asp:Label>
+        <asp:Label ID="tries" runat="server" Text="100"></asp:Label>
+    
+    </div>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Enter a number between 1 and 100"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Enter" />
+        </p>
+        <p>
+            <asp:Label ID="hint" runat="server"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="slabel" runat="server" Text="Score : "></asp:Label>
+            <asp:Label ID="score" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    static int no_try = 100;
+    //int rnd;
+    Random random_number = new Random();
+    static int rnd;
+   
+
+    public void Page_Load(object sender, EventArgs e)
+    {
+
+        if (!IsPostBack)
+        {
+           
+            rnd = random_number.Next(1, 101);
+            //Label4.Text = Convert.ToString(rnd);
+            no_try = 100;
+        }
+    }
+
+
+
+    public void Button1_Click(object sender, EventArgs e)
+    {
+        if(Int32.Parse(TextBox1.Text) < rnd)
+        {
+            hint.Text = "Guessed lower, try again";
+            no_try--;
+            tries.Text = Convert.ToString(no_try);
+        }
+        if(Int32.Parse(TextBox1.Text) > rnd)
+        {
+            hint.Text = "Guessed higher, try again";
+            no_try--;
+            tries.Text = Convert.ToString(no_try);
+        }
+        if(Int32.Parse(TextBox1.Text) == rnd)
+        {
+            hint.Text = "Congratulations !! You've guessed the correct number.";
+            no_try--;
+            tries.Text = Convert.ToString(no_try);
+            score.Text = tries.Text;
+            //tries.Text = Convert.ToString(no_try);
+            
+           
+        }
+        
+    }
+
+ 
+}
